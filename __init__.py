f37c41cadc7b58409c71c50e500a603e04b23903
dshot92 <dshot92@gmail.com>
2024-09-06 20:13:59 +0200

    fix: utils does not need reload

--- __init__.py
+++ __init__.py
@@ -9,28 +9,28 @@ import importlib
 from . import properties, operators, panel, utils
 
 
-
 modules = (
     properties,
     operators,
     panel,
-    utils,
 )
 
 if "bpy" in locals():
     importlib.reload(properties)
     importlib.reload(operators)
     importlib.reload(panel)
-    importlib.reload(utils)
+
 
 def register():
     for module in modules:
         importlib.reload(module)
         module.register()
 
+
 def unregister():
     for module in reversed(modules):
         module.unregister()
+
 
 if __name__ == "__main__":
     register()
